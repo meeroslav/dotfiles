fd1f3468d95c59e3cb8be6fc87cb1c672f71a255
Miroslav Jonas <missing.manual@gmail.com>
2024-02-18 22:04:17 +0100

    fix: add missing methods

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Miro's dotfiles
 
-Configuration files and utilities for Mac based on [paulmillr](https://github.com/paulmillr/dotfiles) dotfiles.
+Configuration files and utilities for Mac based on [alrra](https://github.com/alrra/dotfiles) and [paulmillr](https://github.com/paulmillr/dotfiles) dotfiles.
 
 > Use at your own risk. Please review the configuration files to make sure you are okay with the preferences.
 
--- src/utils.sh
+++ src/utils.sh
@@ -45,6 +45,16 @@ print_warning() {
     print_in_yellow "   [!] $1\n"
 }
 
+print_error() {
+    print_in_red "   [✖] $1 $2\n"
+}
+
+print_error_stream() {
+    while read -r line; do
+        print_error "↳ ERROR: $line"
+    done
+}
+
 ask_for_sudo() {
 
     # Ask for the administrator password upfront.
@@ -74,4 +84,168 @@ answer_is_yes() {
     [[ "$REPLY" =~ ^[Yy]$ ]] \
         && return 0 \
         || return 1
+}
+
+cmd_exists() {
+    command -v "$1" &> /dev/null
+}
+
+execute() {
+
+    local -r CMDS="$1"
+    local -r MSG="${2:-$1}"
+    local -r TMP_FILE="$(mktemp /tmp/XXXXX)"
+
+    local exitCode=0
+    local cmdsPID=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # If the current process is ended,
+    # also end all its subprocesses.
+
+    set_trap "EXIT" "kill_all_subprocesses"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Execute commands in background
+    # shellcheck disable=SC2261
+
+    eval "$CMDS" \
+        &> /dev/null \
+        2> "$TMP_FILE" &
+
+    cmdsPID=$!
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Show a spinner if the commands
+    # require more time to complete.
+
+    show_spinner "$cmdsPID" "$CMDS" "$MSG"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait for the commands to no longer be executing
+    # in the background, and then get their exit code.
+
+    wait "$cmdsPID" &> /dev/null
+    exitCode=$?
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Print output based on what happened.
+
+    print_result $exitCode "$MSG"
+
+    if [ $exitCode -ne 0 ]; then
+        print_error_stream < "$TMP_FILE"
+    fi
+
+    rm -rf "$TMP_FILE"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    return $exitCode
+
+}
+
+kill_all_subprocesses() {
+    local i=""
+
+    for i in $(jobs -p); do
+        kill "$i"
+        wait "$i" &> /dev/null
+    done
+
+}
+
+set_trap() {
+
+    trap -p "$1" | grep "$2" &> /dev/null \
+        || trap '$2' "$1"
+
+}
+
+show_spinner() {
+    local -r FRAMES='/-\|'
+
+    # shellcheck disable=SC2034
+    local -r NUMBER_OR_FRAMES=${#FRAMES}
+
+    local -r CMDS="$2"
+    local -r MSG="$3"
+    local -r PID="$1"
+
+    local i=0
+    local frameText=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Note: In order for the Travis CI site to display
+    # things correctly, it needs special treatment, hence,
+    # the "is Travis CI?" checks.
+
+    if [ "$TRAVIS" != "true" ]; then
+
+        # Provide more space so that the text hopefully
+        # doesn't reach the bottom line of the terminal window.
+        #
+        # This is a workaround for escape sequences not tracking
+        # the buffer position (accounting for scrolling).
+        #
+        # See also: https://unix.stackexchange.com/a/278888
+
+        printf "\n\n\n"
+        tput cuu 3
+
+        tput sc
+
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Display spinner while the commands are being executed.
+
+    while kill -0 "$PID" &>/dev/null; do
+
+        frameText="   [${FRAMES:i++%NUMBER_OR_FRAMES:1}] $MSG"
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Print frame text.
+
+        if [ "$TRAVIS" != "true" ]; then
+            printf "%s\n" "$frameText"
+        else
+            printf "%s" "$frameText"
+        fi
+
+        sleep 0.2
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Clear frame text.
+
+        if [ "$TRAVIS" != "true" ]; then
+            tput rc
+        else
+            printf "\r"
+        fi
+
+    done
+}
+
+mkd() {
+    if [ -n "$1" ]; then
+        if [ -e "$1" ]; then
+            if [ ! -d "$1" ]; then
+                print_error "$1 - a file with the same name already exists!"
+            else
+                print_success "$1"
+            fi
+        else
+            execute "mkdir -p $1" "$1"
+        fi
+    fi
 }
