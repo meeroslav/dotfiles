b21acf59195bedcaa5651059e6f9990fddecd601
Miroslav Jonas <missing.manual@gmail.com>
2024-02-23 12:56:21 +0100

    feat: setup git rsa keys

--- src/installs/git.sh
+++ src/installs/git.sh
@@ -10,3 +10,8 @@ print_in_purple "\n   Git\n\n"
 
 brew_install "Git" "git"
 brew_install "GitHub CLI" "gh"
+
+brew_install "Jq" "jq"
+execute \
+    "ssh-keygen -R github.com && curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> ~/.ssh/known_hosts" \
+    "Setup GitHub RSA SSH Key"
